0a5d7e5210026dab630749de1b76846eade25ed8
shao <ligeard.pro@gmail.com>
2023-12-10 23:02:25 -0500

    🔧 add aliases for paths

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
+//=vite.config.js
+
 import react from "@vitejs/plugin-react"
 import autoprefixer from "autoprefixer"
 import tailwindcss from "tailwindcss"
+import { fileURLToPath, URL } from "url"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+const srcAlias = fileURLToPath(new URL("./src", import.meta.url))
+const componentsAlias = fileURLToPath(new URL("./src/components", import.meta.url))
+const assetsAlias = fileURLToPath(new URL("./src/assets", import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": srcAlias,
+      "@components": componentsAlias,
+      "@assets": assetsAlias,
+    },
+  },
+
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
